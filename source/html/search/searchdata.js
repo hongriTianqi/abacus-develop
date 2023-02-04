4b4e02baa0e6293cf1e2100ae4d7ce6eb5457bac
hongriTianqi <z.hao.1@163.com>
2023-02-04 11:31:48 +0800

    update doxygen at 2023/2/4

--- source/html/search/searchdata.js
+++ source/html/search/searchdata.js
@@ -1,48 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "1_abcdefghijklmnopqrstuvwxyz~",
-  1: "abcdefghijklmnoprstuvwxyz",
-  2: "bcdeghilmprstvw",
-  3: "1abcdefghiklmnoprstuvwxy",
-  4: "_abcdefghijklmnopqrstuvwxyz~",
-  5: "_abcdefghijklmnopqrstuvwxyz",
-  6: "acdefghikmnrstvz",
-  7: "acdfjmp",
-  8: "abcdfghklnoprstuv",
-  9: "cefhilmnoprsw",
-  10: "_cdefhilmnprstwx",
-  11: "m"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "namespaces",
-  3: "files",
-  4: "functions",
-  5: "variables",
-  6: "typedefs",
-  7: "enums",
-  8: "enumvalues",
-  9: "related",
-  10: "defines",
-  11: "pages"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Namespaces",
-  3: "Files",
-  4: "Functions",
-  5: "Variables",
-  6: "Typedefs",
-  7: "Enumerations",
-  8: "Enumerator",
-  9: "Friends",
-  10: "Macros",
-  11: "Pages"
 };
 
