8a6cff7fe8dcdb82d65cfb2f45a73ca9aa8d2a64
hongriTianqi <z.hao.1@163.com>
2024-06-12 10:07:56 +0800

    update output_mulliken_test

--- source/module_io/test/output_mulliken_test.cpp
+++ source/module_io/test/output_mulliken_test.cpp
@@ -9,6 +9,10 @@ template <typename TK>
 class OutputMullikenTest : public testing::Test
 {
   protected:
+    std::vector<std::string> atomLabels = {"Si"};
+    std::vector<int> atomCounts = {1};
+    std::vector<int> orbitalCounts = {13};
+    std::vector<std::vector<int>> lnchiCounts = {{2, 2, 1}};
 };
 
 using MyTypes = ::testing::Types<double, std::complex<double>>;
@@ -16,5 +20,7 @@ TYPED_TEST_SUITE(OutputMullikenTest, MyTypes);
 
 TYPED_TEST(OutputMullikenTest, EmptyWrite)
 {
+    CellIndex cell_index = CellIndex(this->atomLabels, this->atomCounts, this->orbitalCounts, this->lnchiCounts, 1);
+    cell_index.write_orb_info("./");
     EXPECT_EQ(1, 1);
 }
